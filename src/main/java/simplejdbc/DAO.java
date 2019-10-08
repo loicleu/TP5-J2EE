3be4890c302eeca2fb112d21697d61e25c044262
pedago <pedago@pcalb-eb0205.univ-jfc.fr>
2019-10-08 14:16:03 +0200

    Base de donnée TP5

--- src/main/java/simplejdbc/DAO.java
+++ src/main/java/simplejdbc/DAO.java
@@ -29,25 +29,22 @@ public class DAO {
 	 * @throws DAOException
 	 */
 	public int numberOfCustomers() throws DAOException {
-		int result = 0;
+		int resultat = 0;
 
-		String sql = "SELECT COUNT(*) AS NUMBER FROM CUSTOMER";
-		// Syntaxe "try with resources" 
-		// cf. https://stackoverflow.com/questions/22671697/try-try-with-resources-and-connection-statement-and-resultset-closing
-		try (   Connection connection = myDataSource.getConnection(); // Ouvrir une connexion
-			Statement stmt = connection.createStatement(); // On crée un statement pour exécuter une requête
-			ResultSet rs = stmt.executeQuery(sql) // Un ResultSet pour parcourir les enregistrements du résultat
+		String requete = "SELECT COUNT(*) AS NUMBER FROM CUSTOMER";
+		try (   Connection connection = myDataSource.getConnection(); 
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery(requete)
 		) {
-			if (rs.next()) { // Pas la peine de faire while, il y a 1 seul enregistrement
-				// On récupère le champ NUMBER de l'enregistrement courant
-				result = rs.getInt("NUMBER");
+			if (rs.next()) {
+				resultat = rs.getInt("NUMBER");
 			}
 		} catch (SQLException ex) {
 			Logger.getLogger("DAO").log(Level.SEVERE, null, ex);
 			throw new DAOException(ex.getMessage());
 		}
 
-		return result;
+		return resultat;
 	}
 
 	/**
@@ -58,15 +55,13 @@ public class DAO {
 	 */
 	public int deleteCustomer(int customerId) throws DAOException {
 
-		// Une requête SQL paramétrée
-		String sql = "DELETE FROM CUSTOMER WHERE CUSTOMER_ID = ?";
+		String requete = "DELETE FROM CUSTOMER WHERE CUSTOMER_ID = ?";
 		try (   Connection connection = myDataSource.getConnection();
-			PreparedStatement stmt = connection.prepareStatement(sql)
+			PreparedStatement state = connection.prepareStatement(requete);
                 ) {
-                        // Définir la valeur du paramètre
-			stmt.setInt(1, customerId);
+			state.setInt(1, customerId);
 			
-			return stmt.executeUpdate();
+			return state.executeUpdate();
 
 		}  catch (SQLException ex) {
 			Logger.getLogger("DAO").log(Level.SEVERE, null, ex);
@@ -81,25 +76,21 @@ public class DAO {
 	 * @throws DAOException
 	 */
 	public int numberOfOrdersForCustomer(int customerId) throws DAOException {
-		int result = 0;
-
-		// Une requête SQL paramétrée
-		String sql = "SELECT COUNT(*) AS NUMBER FROM PURCHASE_ORDER WHERE CUSTOMER_ID = ?";
+		int resultat = 0;
+		String requete = "SELECT COUNT(*) AS NUMBER FROM PURCHASE_ORDER WHERE CUSTOMER_ID = ?";
 		try (   Connection connection = myDataSource.getConnection();
-			PreparedStatement stmt = connection.prepareStatement(sql)
+			PreparedStatement state = connection.prepareStatement(requete);
                 ) {
-                        // Définir la valeur du paramètre
-			stmt.setInt(1, customerId);
-
-			try (ResultSet rs = stmt.executeQuery()) {
-				rs.next(); // On a toujours exactement 1 enregistrement dans le résultat
-				result = rs.getInt("NUMBER");
+			state.setInt(1, customerId);
+			try (ResultSet rs = state.executeQuery()) {
+				rs.next();
+				resultat = rs.getInt("NUMBER");
 			}
 		}  catch (SQLException ex) {
 			Logger.getLogger("DAO").log(Level.SEVERE, null, ex);
 			throw new DAOException(ex.getMessage());
 		}
-		return result;
+		return resultat;
 	}
 
 	/**
@@ -110,27 +101,26 @@ public class DAO {
 	 * @throws DAOException
 	 */
 	public CustomerEntity findCustomer(int customerID) throws DAOException {
-		CustomerEntity result = null;
+		CustomerEntity resultat = null;
 
-		String sql = "SELECT * FROM CUSTOMER WHERE CUSTOMER_ID = ?";
-		try (Connection connection = myDataSource.getConnection(); // On crée un statement pour exécuter une requête
-			PreparedStatement stmt = connection.prepareStatement(sql)) {
+		String requete = "SELECT * FROM CUSTOMER WHERE CUSTOMER_ID = ?";
+		try (Connection connection = myDataSource.getConnection(); 
+			PreparedStatement state = connection.prepareStatement(requete);) {
 
-			stmt.setInt(1, customerID);
-			try (ResultSet rs = stmt.executeQuery()) {
-				if (rs.next()) { // On a trouvé
+			state.setInt(1, customerID);
+			try (ResultSet rs = state.executeQuery()) {
+				if (rs.next()) {
 					String name = rs.getString("NAME");
 					String address = rs.getString("ADDRESSLINE1");
-					// On crée l'objet "entity"
-					result = new CustomerEntity(customerID, name, address);
-				} // else on n'a pas trouvé, on renverra null
+					resultat = new CustomerEntity(customerID, name, address);
+				} 
 			}
 		}  catch (SQLException ex) {
 			Logger.getLogger("DAO").log(Level.SEVERE, null, ex);
 			throw new DAOException(ex.getMessage());
 		}
 
-		return result;
+		return resultat;
 	}
 
 	/**
@@ -141,24 +131,21 @@ public class DAO {
 	 * @throws DAOException
 	 */
 	public List<CustomerEntity> customersInState(String state) throws DAOException {
-		List<CustomerEntity> result = new LinkedList<>(); // Liste vIde
+		List<CustomerEntity> resultat = new LinkedList<>();
 
 		String sql = "SELECT * FROM CUSTOMER WHERE STATE = ?";
 		try (Connection connection = myDataSource.getConnection();
-			PreparedStatement stmt = connection.prepareStatement(sql)) {
+			PreparedStatement statem = connection.prepareStatement(sql)) {
 
-			stmt.setString(1, state);
+			statem.setString(1, state);
 
-			try (ResultSet rs = stmt.executeQuery()) {
-				while (rs.next()) { // Tant qu'il y a des enregistrements
-					// On récupère les champs nécessaires de l'enregistrement courant
+			try (ResultSet rs = statem.executeQuery()) {
+				while (rs.next()) {
 					int id = rs.getInt("CUSTOMER_ID");
 					String name = rs.getString("NAME");
 					String address = rs.getString("ADDRESSLINE1");
-					// On crée l'objet entité
 					CustomerEntity c = new CustomerEntity(id, name, address);
-					// On l'ajoute à la liste des résultats
-					result.add(c);
+					resultat.add(c);
 				}
 			}
 		}  catch (SQLException ex) {
@@ -166,7 +153,7 @@ public class DAO {
 			throw new DAOException(ex.getMessage());
 		}
 
-		return result;
+		return resultat;
 
 	}
 
